0c1b55bc85aab97b6f5797cf1214ef930a4b46a7
tobiassilalahi <tobias.silalahi@gmail.com>
2025-04-28 14:21:11 +0700

    added exception and logger

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys 
+from logger import logging
 
 def error_message_detail(error, error_details:sys):
     _,_,exc_tb = error_details.exc_info()
@@ -9,9 +10,18 @@ def error_message_detail(error, error_details:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error, error_message, error_details:sys):
-        super.__init__(error_message)
-        self.error_message = error_message_detail(error=error, error_details=error_details)
+    def __init__(self, error_message, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_details=error_details)
 
     def __str__(self):
         return self.error_message
+    
+
+
+# if __name__ == "__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by zero error")
+#         raise CustomException(e, sys)
--- src/logger.py
+++ src/logger.py
@@ -8,3 +8,16 @@ logs_path = os.path.join(os.getcwd(), "logs",LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Configure what is written in my log file
+# What does level mean in here?
+    # it printed out INFO (thats the level name)
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+
+# if __name__ =="__main__":
+#     logging.info("Logging has started")
